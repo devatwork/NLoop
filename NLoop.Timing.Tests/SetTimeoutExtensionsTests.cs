0fb504be093757054fc08df515ecaa04b5170272
Bert Willems <b.willems@premotion.nl>
2013-08-17 13:16:52 +0200

    + setTimeout

---
+++ NLoop.Timing.Tests/SetTimeoutExtensionsTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Threading;
+using NLoop.Core;
+using NUnit.Framework;
+
+namespace NLoop.Timing.Tests
+{
+	[TestFixture]
+	public class SetTimeoutExtensionsTests
+	{
+		[Test]
+		public void SetTimeoutParameterChecking()
+		{
+			// arrange
+			var loop = new EventLoop();
+			loop.Start(() => { });
+			var callback = new Action(() => { });
+			var timeout = TimeSpan.FromMilliseconds(100);
+
+			// assert
+			Assert.That(() => ((EventLoop) null).SetTimeout(callback, timeout), Throws.InstanceOf<ArgumentNullException>());
+			Assert.That(() => loop.SetTimeout(null, timeout), Throws.InstanceOf<ArgumentNullException>());
+		}
+		[Test]
+		public void SetTimeout()
+		{
+			// arrange
+			var loop = new EventLoop();
+			loop.Start(() => { });
+			var callbackinvoked = new ManualResetEvent(false);
+			var callback = new Action(() => callbackinvoked.Set());
+			var timeout = TimeSpan.FromMilliseconds(100);
+
+			// act
+			var cancelTimeout = loop.SetTimeout(callback, timeout);
+
+			// assert
+			Assert.That(cancelTimeout, Is.Not.Null);
+			Assert.That(callbackinvoked.WaitOne(timeout + timeout), Is.True);
+		}
+		[Test]
+		public void CancelTimeout()
+		{
+			// arrange
+			var loop = new EventLoop();
+			loop.Start(() => { });
+			var callbackinvoked = new ManualResetEvent(false);
+			var callback = new Action(() => callbackinvoked.Set());
+			var timeout = TimeSpan.FromMilliseconds(100);
+
+			// act
+			var cancelTimeout = loop.SetTimeout(callback, timeout);
+			cancelTimeout.Dispose();
+
+			// assert
+			Assert.That(callbackinvoked.WaitOne(timeout + timeout), Is.False);
+		}
+	}
+}
---
+++ NLoop.Timing/SetTimeoutExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+using NLoop.Core;
+
+namespace NLoop.Timing
+{
+	/// <summary>
+	/// Contains the timeout extension methods for the <see cref="EventLoop"/>.
+	/// </summary>
+	public static class SetTimeoutExtensions
+	{
+		/// <summary>
+		/// Invokes the <paramref name="callback"/> after <paramref name="timeout"/> elapsed.
+		/// </summary>
+		/// <param name="eventLoop">The <see cref="EventLoop"/> on which the timeout event will be executed.</param>
+		/// <param name="callback">The callback which to invoke on timeout.</param>
+		/// <param name="timeout">The timeout which to wait before <paramref name="callback"/> should be executed.</param>
+		/// <returns>Returns a <see cref="IDisposable"/> which cancels the timer.</returns>
+		/// <exception cref="ArgumentNullException">Thrown if one of the parameters is null.</exception>
+		public static IDisposable SetTimeout(this EventLoop eventLoop, Action callback, TimeSpan timeout)
+		{
+			// validate arguments
+			if (eventLoop == null)
+				throw new ArgumentNullException("eventLoop");
+			if (callback == null)
+				throw new ArgumentNullException("callback");
+
+			// create the timeout
+			return new Timer(state => eventLoop.Add(callback), null, timeout, Timeout.InfiniteTimeSpan);
+		}
+	}
+}
