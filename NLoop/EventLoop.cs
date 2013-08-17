ae0fa8160b3605bf8ad5d356839e9dfabaf4ee00
Bert Willems <b.willems@premotion.nl>
2013-08-17 10:59:02 +0200

    + basic event loop

---
+++ NLoop/EventLoop.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
+
+namespace NLoop
+{
+	/// <summary>
+	/// Implements the core API of the event loop.
+	/// </summary>
+	public class EventLoop
+	{
+		/// <summary>
+		/// Holds all the callbacks to event handlers which need to be invoked in this event loop.
+		/// </summary>
+		private readonly ConcurrentQueue<Action> callbackQueue = new ConcurrentQueue<Action>();
+		/// <summary>
+		/// Holds the <see cref="EventLoopWorker"/> used by this event loop.
+		/// </summary>
+		private EventLoopWorker worker;
+		/// <summary>
+		/// A value which indicates the started state. 0 indicates not started, 1 indicates starting or started.
+		/// </summary>
+		private int startedState;
+		/// <summary>
+		/// Gets a flag indicating whether this event loop was already started or not.
+		/// </summary>
+		public bool WasStarted
+		{
+			get { return Thread.VolatileRead(ref startedState) == 1; }
+		}
+		/// <summary>
+		/// Adds a new <paramref name="callback" /> to the event loop.
+		/// </summary>
+		/// <param name="callback">The callback which to add to this event loop.</param>
+		/// <exception cref="ArgumentNullException">Thrown if <paramref name="callback"/> is null.</exception>
+		public void Add(Action callback)
+		{
+			// validate arguments
+			if (callback == null)
+				throw new ArgumentNullException("callback");
+
+			// enqueue the callback
+			callbackQueue.Enqueue(callback);
+		}
+		/// <summary>
+		/// Starts this event loop and add the given <paramref name="callback"/> to it to execute first.
+		/// </summary>
+		/// <param name="callback"></param>
+		public void Start(Action callback)
+		{
+			// validate arguments
+			if (callback == null)
+				throw new ArgumentNullException("callback");
+
+			// Attempt to move the started state from 0 to 1. If successful, we can be assured that
+			// this thread is the first thread to do so, and can safely start this event loop.
+			if (Interlocked.CompareExchange(ref startedState, 1, 0) != 0)
+				throw new InvalidOperationException("This eventloop was already started");
+
+			// add the initial callback to this loop
+			Add(callback);
+
+			// create the event loop worker
+			worker = new EventLoopWorker(NextCallback);
+
+			// start the worker
+			worker.Start();
+		}
+		/// <summary>
+		/// Tries to dequeue the next callback from the <see cref="callbackQueue"/>.
+		/// </summary>
+		/// <returns>Returns the callback if there is one, otherwise null.</returns>
+		private Action NextCallback()
+		{
+			Action callback;
+			return callbackQueue.TryDequeue(out callback) ? callback : null;
+		}
+	}
+}
---
+++ NLoop/EventLoopWorker.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NLoop
+{
+	/// <summary>
+	/// Implements the worker for the <see cref="EventLoop"/>. The worker spins and processes the <see cref="EventLoop.callbackQueue"/>.
+	/// </summary>
+	public class EventLoopWorker
+	{
+		/// <summary>
+		/// Gets the next callback from the <see cref="EventLoop.callbackQueue"/>.
+		/// </summary>
+		private readonly Func<Action> nextCallback;
+		/// <summary>
+		/// Task on which the callbacks are invoked.
+		/// </summary>
+		private readonly Task worker;
+		/// <summary>
+		/// Synchronizes the stopping of the <see cref="worker"/> and the <see cref="EventLoop"/>.
+		/// </summary>
+		private readonly ManualResetEvent stopHandle = new ManualResetEvent(false);
+		/// <summary>
+		/// Constructs a new event loop worker.
+		/// </summary>
+		/// <param name="nextCallback">A method which to invoke to get the next callack from the <see cref="EventLoop.callbackQueue"/>.</param>
+		/// <exception cref="ArgumentNullException">Thrown if <paramref name="nextCallback"/> is null.</exception>
+		public EventLoopWorker(Func<Action> nextCallback)
+		{
+			// validate arguments
+			if (nextCallback == null)
+				throw new ArgumentNullException("nextCallback");
+
+			// keep a reference to the event loop
+			this.nextCallback = nextCallback;
+
+			// create the new worker
+			worker = Task.Factory.StartNew(() => { });
+		}
+		/// <summary>
+		/// Starts doing work on this <see cref="EventLoopWorker"/>.
+		/// </summary>
+		public void Start()
+		{
+			// first clear the stop handle, which enables the worker loop to rung
+			stopHandle.Reset();
+
+			// start doing th work
+			worker.ContinueWith(Work);
+		}
+		/// <summary>
+		/// Stops doing work.
+		/// </summary>
+		public void Stop()
+		{
+			stopHandle.Set();
+		}
+		/// <summary>
+		/// Implements the event loop.
+		/// </summary>
+		/// <param name="incomingTask">The incoming <see cref="Task"/>.</param>
+		private void Work(Task incomingTask)
+		{
+			// validate arguments
+			if (incomingTask == null)
+				throw new ArgumentNullException("incomingTask");
+
+			// start an endless loop
+			while (true)
+			{
+				// check if the stop handle has been set, indicating we should stop doing work
+				if (stopHandle.WaitOne(0))
+					break;
+
+				// get a callback from the event loop's callback queue
+				var callback = nextCallback();
+
+				// if no callback was returned, there is nothing to do
+				// TODO: make this more efficient with a wait handle?
+				if (callback == null)
+					continue;
+
+				// invoke the callback
+				callback();
+			}
+
+			// dispose the incoming task
+			incomingTask.Dispose();
+		}
+	}
+}
